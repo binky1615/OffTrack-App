1f79b20daad873cc9dc7fbe9ecb11d7a9fa7be31
valentijn <9021853@student.zadkine.nl>
2025-03-07 11:16:44 +0100

    opdracht 1, 2 and 3

--- app/screens/auth/login.jsx
+++ app/screens/auth/login.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { Text, View, ImageBackground, Image, StyleSheet, Pressable, TextInput } from "react-native";
 import { useRouter } from 'expo-router'
 
 export default function login() {
@@ -6,18 +6,83 @@ export default function login() {
     const router = useRouter()
 
   return (
-    <View style={styles.container}>
-      <Text>login</Text>
-      <Button title="Login" onPress={() => router.back()} />
-    </View>
+    <ImageBackground
+      style={styles.imgbackground}
+      resizeMode="cover"
+      source={require("../../../assets/images/loginscreen.png")}
+    >
+      <View style={styles.imgbackground}>
+        <Image
+          style={styles.logo}
+          source={require("../../../assets/images/offtrack-logo.png")}
+        />
+        <View style={styles.buttonContainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Email"
+            keyboardType="email-address"
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Password"
+            secureTextEntry={true}
+          />
+          <Pressable style={styles.button} onPress={() => router.push("/screens/tabs/home")}>
+            <Text style={styles.buttonText}>LOGIN</Text>
+          </Pressable>
+          {/* FIXED Pressable TEXT ERROR */}
+          <Pressable onPress={() => router.push("/screens/auth/register")}>
+            <Text style={styles.linktext}>Don't have an account? Sign up</Text>
+          </Pressable>
+        </View>
+      </View>
+    </ImageBackground>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
+    imgbackground: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+        justifyContent: "space-between",
+        alignItems: "center",
+      },
+
+      logo: {
+        width: 150,
+        resizeMode: "contain",
+        right: 75,
+      },
+
+      linktext: {
+        paddingTop: 20,
+        color: "white",
+        textAlign: "center",
+      },
+
+      textInput: {
+        backgroundColor: "white",
+        borderRadius: 50,
+      },
+
+      buttonContainer: {
+        width: "100%",
+        gap: 10,
+        height: "fit-content",
+        marginBottom: 34,
+      },
+    
+      button: {
+        backgroundColor: "black",
+        borderRadius: 16,
+        padding: 10,
+        paddingLeft: 125,
+        paddingRight: 125,
+        alignItems: "center",
+      },
+    
+      buttonText: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "bold",
+      },
 })
--- app/screens/auth/register.jsx
+++ app/screens/auth/register.jsx
@@ -1,23 +1,92 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { Text, View, ImageBackground, Image, StyleSheet, Pressable, TextInput } from "react-native";
 import { useRouter } from 'expo-router'
 
-export default function register() {
-
-    const router = useRouter()
+export default function login() {
+  const router = useRouter()
 
   return (
-    <View style={styles.container}>
-      <Text>login</Text>
-      <Button title="register" onPress={() => router.back()} />
-    </View>
+    <ImageBackground
+      style={styles.imgbackground}
+      resizeMode="cover"
+      source={require("../../../assets/images/registerscreen.png")}
+    >
+      <View style={styles.imgbackground}>
+        <Image
+          style={styles.logo}
+          source={require("../../../assets/images/offtrack-logo.png")}
+        />
+        <View style={styles.buttonContainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Email"
+            keyboardType="email-address"
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Password"
+            secureTextEntry={true}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Confirm Password"
+            secureTextEntry={true}
+          />
+          <Pressable style={styles.button} onPress={() => router.push("/screens/tabs/home")}>
+            <Text style={styles.buttonText}>create account</Text>
+          </Pressable>
+          <Pressable onPress={() => router.push("/screens/auth/login")}>
+            <Text style={styles.linktext}>Don't have an account? Sign up</Text>
+          </Pressable>
+        </View>
+      </View>
+    </ImageBackground>
   )
 }
 
+
 const styles = StyleSheet.create({
-    container: {
+    imgbackground: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+        justifyContent: "space-between",
+        alignItems: "center",
+      },
+
+      logo: {
+        width: 150,
+        resizeMode: "contain",
+        right: 75,
+      },
+
+      linktext: {
+        paddingTop: 20,
+        color: "black",
+        textAlign: "center",
+      },
+
+      textInput: {
+        backgroundColor: "white",
+        borderRadius: 50,
+      },
+
+      buttonContainer: {
+        width: "100%",
+        gap: 10,
+        height: "fit-content",
+        marginBottom: 34,
+      },
+    
+      button: {
+        backgroundColor: "black",
+        borderRadius: 16,
+        padding: 10,
+        paddingLeft: 120,
+        paddingRight: 120,
+        alignItems: "center",
+      },
+    
+      buttonText: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "bold",
+      },
 })
